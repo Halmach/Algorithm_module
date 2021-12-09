c726345afb99a995f226df179cc76ae1b6292424
Khalmatov <large000@yandex.ru>
2021-12-09 23:26:59 +0300

    Добавьте файлы проекта.

---
+++ Algorithm_module/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Algorithm_module
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Greetings();
+        }
+
+        static void Greetings()
+        {
+            Console.WriteLine("Как Вас зовут?");
+            var name = Console.ReadLine();
+            var greetings = "Привет," + name;
+            Console.WriteLine(greetings);
+        }
+    }
+}
