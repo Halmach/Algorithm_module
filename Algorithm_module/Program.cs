15787ae7e320e4269052f4eebd1826d68d501b54
Khalmatov <large000@yandex.ru>
2021-12-09 23:40:39 +0300

    Задание 12.1.3

--- Algorithm_module/Program.cs
+++ Algorithm_module/Program.cs
@@ -7,6 +7,7 @@ namespace Algorithm_module
         static void Main(string[] args)
         {
             Greetings();
+            ReadArray();
         }
 
         static void Greetings()
@@ -15,6 +16,28 @@ namespace Algorithm_module
             var name = Console.ReadLine();
             var greetings = "Привет," + name;
             Console.WriteLine(greetings);
+        }
+
+        static void ReadArray()
+        {
+            Console.WriteLine("Сколько элементов будет в массиве?");
+            int count = 0;
+            string[] array;
+            try
+            {
+                int.TryParse(Console.ReadLine(), out count);
+                array = new string[count];
+                for(int i = 0; i < array.Length; i++)
+                {
+                    Console.WriteLine($"Заполнить {i} элемент массива");
+                    array[i] = Console.ReadLine(); 
+                }
+                Console.WriteLine("Все элементы записаны");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }         
         }
     }
 }
