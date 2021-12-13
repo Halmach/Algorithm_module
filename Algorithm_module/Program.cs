3b92063dcc1c6588da62839217f9ecca56da88de
Khalmatov <large000@yandex.ru>
2021-12-14 00:52:47 +0300

    Задание 12.3.1

--- Algorithm_module/Program.cs
+++ Algorithm_module/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Algorithm_module
 {
@@ -8,7 +9,28 @@ namespace Algorithm_module
         {
             // Greetings();
             //  ReadArray();
-            CheckAge();
+            //CheckAge();
+            var rnd = new Random();
+
+            int[] mas = new int[10000];
+            int[] mas2 = new int[mas.Length];
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = rnd.Next(-90000, 1230000);
+            }
+
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas2[i] = mas[i];
+            }
+
+            BubbleSort(mas2);
+            Array.Reverse(mas2);
+            //Console.WriteLine(mas);
+            for (int i = 0; i < mas.Length; i++)
+            {
+                if(BinarySearch(ref mas2, mas[i]) == -1) Console.WriteLine(i);
+            }
         }
 
         private static void Greetings() // линейный алгоритм
@@ -54,6 +76,68 @@ namespace Algorithm_module
             {
                 Console.WriteLine("Пользователи младше 14 лет не могут быть зарегистрированы");
             }
+        }
+
+        private static int BinarySearch(ref int[] mas, int requiredElement)
+        {
+            int left = 0;
+            int right = mas.Length;
+
+            for (int i = left; i <= right; i++)
+            {
+                var middle = (right + left) / 2 + 1;
+                if (mas[middle] == requiredElement)
+                {
+            //        Console.WriteLine("Найденое значение = " + mas[middle]);
+                    return middle;
+                }
+
+                if (middle == 1)
+                {
+                    if(mas[middle - 1] == requiredElement)
+                    {
+             //           Console.WriteLine("Найденое значение = " + mas[middle - 1]);
+                        return middle - 1;
+                    }
+                }
+
+                if (mas[middle] < requiredElement)
+                {
+                    i = left;
+                    right = middle - 1;
+                }
+
+                if (mas[middle] > requiredElement)
+                {
+                    left = middle - 1;
+                }
+            }
+
+            return -1;
+        }
+
+        private static void Swap(ref int e1, ref int e2)
+        {
+            var temp = e1;
+            e1 = e2;
+            e2 = temp;
+        }
+
+        private static int[] BubbleSort(int[] array)
+        {
+            var len = array.Length;
+            for (var i = 1; i < len; i++)
+            {
+                for (var j = 0; j < len - i; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        Swap(ref array[j], ref array[j + 1]);
+                    }
+                }
+            }
+
+            return array;
         }
     }
 }
